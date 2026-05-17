b865b431494d9ab7cd92f1c08667716a41157560
Elamin Haddouche <elaminhaddouche@protonmail.com>
2026-05-17 16:27:18 +0200

    fix extra padding in loading

--- apps/www/src/app/docs/[id]/loading.tsx
+++ apps/www/src/app/docs/[id]/loading.tsx
@@ -2,7 +2,7 @@ import Skeleton from "@registry/components/react/feedback/skeleton";
 
 export default function Loading() {
   return (
-    <div className="flex flex-col gap-8 py-4 px-4 pt-12">
+    <div className="flex flex-col gap-8">
       <div className="flex flex-col gap-2.5">
         <Skeleton className="h-8 w-40" />
         <Skeleton className="h-7 w-[90%]" />
